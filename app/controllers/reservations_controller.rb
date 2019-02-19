81974ad8f82fc81f4b545a2cb5bdd7cef1390cc2
Julien LUCIANI <korstg@gmail.com>
2019-02-19 15:22:28 +0100

    create reservations view/controller view+form

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,11 @@
 class ReservationsController < ApplicationController
-
+  before_action :set_experience
   def index
     @reservations = Reservation.all
+  end
+
+  def new
+    @reservation = Reservation.new
   end
 
   def show
@@ -9,9 +13,21 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @reservation = Reservation.new
-    @reservation.save
+    @reservation = @experience.reservations.new(reservation_params)
+    if @reservation.save
+      redirect_to experience_path(params[:experience_id])
+    else
+      render :new
+    end
   end
 
+  private
 
+  def reservation_params
+    params.require(:reservation).permit(:experience_id, :participants_number, :start_at, :end_at)
+  end
+
+  def set_experience
+    @experience = Experience.find(params[:experience_id])
+  end
 end
