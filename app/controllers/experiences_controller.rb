ce34810654a3346a4ee947effcbee83938c5223b
Julien LUCIANI <korstg@gmail.com>
2019-02-19 11:56:38 +0100

    create view => experiences/show update experiences_controller

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,2 +1,17 @@
 class ExperiencesController < ApplicationController
+
+  # def new
+
+  # end
+
+  def show
+    @experience = Experience.find(params[:id])
+  end
+
+
+  # private
+
+  # def experience_params
+  #   params.require(:experience).permit(:name, :description, :photo, :price)
+  # end
 end
