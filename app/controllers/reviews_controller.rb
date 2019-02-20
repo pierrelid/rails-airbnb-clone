a4a5d2bdc546606d83270f14795ed5da80b72b74
jean marc graufogel <gfl.jm2@wanadoo.fr>
2019-02-20 10:37:00 +0100

    change review not done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,29 +1,22 @@
 class ReviewsController < ApplicationController
-  before_action :set_reservation, only: [:new, :create]
 
   def new
-    @review = Review.new
+    @review = Review.find(params[:review_id])
   end
 
   def create
     @review = Review.new(review_params)
     @review.reservation = @review
-    @reservation.user = current_user
+    @review.user = current_user
     if @review.save
-      redirect_to reservation_path(params[:reservation_id])
+      redirect_to review_path
     else
       render new
     end
   end
 
   private
-
-  def set_reservation
-    @reservation = Reservation.find(params[:reservation_id])
-  end
-
   def review_params
-    params.require(:review).permit(:reservation_id, :comment)
+    params.require(:review).permit(:reservation, :comment, :rating)
   end
 end
-
