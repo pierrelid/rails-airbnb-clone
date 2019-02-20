c4d82e838328135dc15f5f592d73f1e79629c31c
Fanny Maurel <fannymaurel@outlook.com>
2019-02-20 14:11:39 +0100

    Delete migration

--- db/migrate/20190220100140_add_coordinates_to_experiences.rb
+++
@@ -1,6 +0,0 @@
-class AddCoordinatesToExperiences < ActiveRecord::Migration[5.2]
-  def change
-    add_column :experiences, :latitude, :float
-    add_column :experiences, :longitude, :float
-  end
-end
